42d4e71a944621d93f9a892ac4de825fcba6f6f8
root <root@ip-192-168-1-82.us-east-2.compute.internal>
2020-02-26 06:36:18 +0000

    webhoo test

--- src/main/java/com/dl/proj1/App.java
+++ src/main/java/com/dl/proj1/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World-webhook" );
-	System.out.println( "webhook" );
+	System.out.println( "webhook test" );
 
     }
 }
